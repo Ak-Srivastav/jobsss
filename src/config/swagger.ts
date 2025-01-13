3450f2507a477cfd8b29837a10be702f55f603a2
Amitkumar Srivastav <amit.edu@outlook.com>
2025-01-13 20:50:04 +0530

    swagger update

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,8 +10,41 @@ const options = {
             version: '1.0.0',
             description: 'API documentation for the Job Board backend',
         },
+        components: {
+            schemas: {
+                Job: {
+                    type: 'object',
+                    properties: {
+                        id: {
+                            type: 'integer',
+                            description: 'The job ID',
+                        },
+                        title: {
+                            type: 'string',
+                            description: 'The job title',
+                        },
+                        company: {
+                            type: 'string',
+                            description: 'The company name',
+                        },
+                        location: {
+                            type: 'string',
+                            description: 'The job location',
+                        },
+                        salary: {
+                            type: 'integer',
+                            description: 'The job salary',
+                        },
+                        description: {
+                            type: 'string',
+                            description: 'The job description',
+                        },
+                    },
+                },
+            },
+        },
     },
-    apis: ['./src/routes/*.ts'], // Path to the API docs
+    apis: ['./src/routes/*.ts'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
